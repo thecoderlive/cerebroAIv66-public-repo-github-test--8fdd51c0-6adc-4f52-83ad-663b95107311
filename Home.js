dc9721968c68cec81e377ee6c026a6c0cab50304
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-30 12:20:28 +0530

    style changes on Home Screen.

--- Home.js
+++ Home.js
@@ -59,7 +59,7 @@ export default Home;
 const styles = StyleSheet.create({
     "center": {
         "flex": 1,
-        "justifyContent": "center",
-        "alignItems": "center"
+        "alignItems": "center",
+        "justifyContent": "center"
     }
 });
